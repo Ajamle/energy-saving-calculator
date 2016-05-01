2d90dbd3f2ea50b89f86fc8d947e6c20f9fe3842
Ajamle <elmajab@gmail.com>
2016-05-01 11:26:10 +0200

    changes as of 01 May

--- dialog.cpp
+++ dialog.cpp
@@ -1,5 +1,6 @@
 #include "dialog.h"
 #include "ui_dialog.h"
+#include <QMessageBox>
 
 Dialog::Dialog(QWidget *parent) :
     QDialog(parent),
@@ -9,24 +10,23 @@ Dialog::Dialog(QWidget *parent) :
     ui->comboBox->addItem("Water pipe");
     ui->comboBox->addItem("Charger");
     ui->comboBox->addItem("TV light");
+
+    ui->comboBox_2->addItem("30 seconds");
+    ui->comboBox_2->addItem("1 minute");
+    ui->comboBox_2->addItem("1 hour");
+    ui->comboBox_2->addItem("1 day");
+    ui->comboBox_2->addItem("1 month");
+
+    ui->comboBox_3->addItem("per day");
+    ui->comboBox_3->addItem("per week");
+    ui->comboBox_3->addItem("per month");
+    ui->comboBox_3->addItem("per year");
+
+    ui->comboBox_4->addItem("1,17 BAM");
+    ui->comboBox_4->addItem("0,71 BAM");
+    ui->comboBox_4->addItem("0,37 BAM");
 }
-ui(new Ui::Dialog)
-{
-ui->setupUi(this);
-ui->comboBox->addItem("30 seconds");
-ui->comboBox->addItem("1 minute");
-ui->comboBox->addItem("1 hour");
-ui->comboBox->addItem("1 day");
-ui->comboBox->addItem("1 month");
-}
-ui(new Ui::Dialog)
-{
-ui->setupUi(this);
-ui->comboBox->addItem("per day");
-ui->comboBox->addItem("per week");
-ui->comboBox->addItem("per month");
-ui->comboBox->addItem("per year");
-}
+
 Dialog::~Dialog()
 {
     delete ui;
@@ -34,7 +34,5 @@ Dialog::~Dialog()
 
 void Dialog::on_pushButton_clicked()
 {
-    QMessageBox::information(this, "1,17 BAM",
-                             "0,71 BAM"
-                             "0,37 BAM");
+    QMessageBox::information(this, "Enter", "This is your saving");
 }
